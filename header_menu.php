b08ab0827722ed0dd81a962e13ce181e5fc468a2
dapras <dapras.solo@gmail.com>
2019-02-08 15:47:06 +0700

    leftmenu

--- header_menu.php
+++ header_menu.php
@@ -14,46 +14,11 @@
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top"  style="margin-left: -0px;">
       <div class="navbar-custom-menu" style="float: left;">
-        <img src="dist/img/user-160x160.jpg" class="user-image" alt="User Image" height="60" width="60" style="padding: 5px 5px 5px 5px;">
-              <span style="font-size: 20px; color: #FFFFFF;">Sistem Informasi Pembayaran Sekolah - SMKS Muhammadiyah 9 Gambiran</span>
+        <!--<img src="dist/img/user-160x160x.jpg" class="user-image" alt="User Image" height="60" width="60" style="padding: 5px 5px 5px 5px;">-->
+              <span style="font-size: 20px; color: #FFFFFF;">&nbsp;&nbsp;&nbsp;&nbsp;Trucking Solutions Indonesia</span>
         
       </div>  
     </nav>
-   <div id='cssmenu'>
-<ul>
-   <li class='has-sub'><a href='#'>Master</a>
-      <ul>
-         <li><a href='?p=tahunakademik'>Tahun Akademik</a></li>
-         <li><a href='?p=kelas'>Kelas</a></li>
-         <li><a href='?p=jurusan'>Jurusan</a></li>
-         <li><a href='?p=siswa'>Siswa</a></li>
-         <li><a href='?p=jabatan'>Jabatan</a></li>
-         <li><a href='?p=golongan'>Golongan</a></li>
-         <li><a href='?p=ruang'>Ruang</a></li>
-         <li><a href='?p=bidang_studi'>Bidang Studi</a></li>
-         <li><a href='?p=jenis_pembayaran'>Jenis Pembayaran</a></li>
-         <li><a href='?p=jenjang_pendidikan'>Jenjang Pendidikan</a></li>
-         <li><a href='?p=pegawai_status'>Pegawai Status</a></li>
-         <li><a href='?p=pegawai'>Pegawai</a></li>
-         <li><a href='?p=siswa_kelas'>Siswa Kelas</a></li>
-         <li><a href='?p=jenjang_bayar'>Jenjang Pembayaran</a></li>  
-      </ul>
-    </li>
-    <li><a href='?p=wajib_bayar'>Tagihan Siswa</a></li>   
-    <li><a href='?p=siswa_bayar'>Pembayaran Siswa</a></li> 
-   
-   <!--
-   <li class='has-sub'><a href='#'>Laporan</a>
-      <ul>
-         <li><a href='#'>Dokter, Bidan, Fisio</a></li>
-         <li><a href='#'>Farmasi</a></li>
-         <li><a href='#'>Keuangan</a></li>
-      </ul>
-   </li>
- -->
-   <li class='has-sub'><a href='logout.php'>Logout</a>
-</ul>
-</div>
 <script>
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
--- menu_left.php
+++ menu_left.php
@@ -2,99 +2,66 @@
   <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
-      <div class="user-panel">
-        <div class="pull-left image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-        </div>
-        <div class="pull-left info">
-          <p>PENDAFTARAAN</p>
-          <a href="#"><i class="fa fa-circle text-success"></i> ANDRI, SE</a>
-        </div>
-      </div>
       
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
-        <li class="header"></li>
-        <!-- PENDAFTARAAN -->
-        <li><a href="?p=pendaftaran"><i class="fa fa-book"></i><span>PENDAFTARAAN</span></a></li>
-        <li><a href="?p=pasienrajal"><i class="fa fa-book"></i><span>PASIEN RAWAT JALAN</span></a></li>
-        <li><a href="?p=datapasien"><i class="fa fa-book"></i><span>DATA PASIEN</span></a></li>
-        <li><hr></li>
-        <!-- IGD -->
-        <li><a href="?p=rekammedikrajal"><i class="fa fa-book"></i><span>RIWAYAT PASIEN RAJAL</span></a></li>
-        <li><a href="?p=rekammedikranap"><i class="fa fa-book"></i><span>RIWAYAT PASIEN RANAP</span></a></li>
-        <li><hr></li>
-        <!--<li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>DATA PASIEN IGD</span></a></li>
-        <li><hr></li>
-         ADMISSION -->
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>PENDAFTARAN RAWAT INAP</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>DATA PASIEN RAWAT INAP</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>DATA KAMAR</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>DATA PASIEN RAWAT JALAN</span></a></li>
-        <li><hr></li>
-        <!-- RANAP -->
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>DATA RAWAT RANAP</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>DATA KAMAR</span></a></li>
-        <li><hr></li>        
-        <!-- APOTEK -->
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>DATA PASIEN RAWAT JALAN</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>DATA PASIEN IGD</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>DATA PASIEN RAWAT INAP</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>DATA BARANG</span></a></li>
-        <li><hr></li>        
-        <!-- LABORATORIUM -->
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>ORDER LAB</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>PEMERIKSAAN LAB</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>HASIL LAB</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>PASIEN RAJAL</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>PASIEN IGD</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>PASIEN RANAP</span></a></li>
-        <li><hr></li>        
-        <!-- LOGISTIK -->
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>DATA SUPPLIER</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>PENERIMAAN BARANG SUPPLIER</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>PENGEMBALIAN BARANG SUPPLIER</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>STOK BARANG</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>MASTER BARANG</span></a></li>
-        <li><hr></li>        
-        <!-- PEMBAYARAN -->
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>BILL PASIEN RAWAT JALAN</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>BILL PASIEN IGD</span></a></li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>BILL PASIEN RAWAT INAP</span></a></li>
-        <!--<li class="treeview">
+        <li>&nbsp;
+        </li>
+        <?php if ($seskdlvl=='partner'){?>
+        <li class="treeview">
           <a href="#">
-            <i class="fa fa-book"></i> <span>Multilevel</span>
+            <i class="fa fa-laptop"></i>
+            <span>Overview</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="#"><i class="fa fa-files-o"></i> Level One</a></li>
-            <li class="treeview">
-              <a href="#"><i class="fa fa-files-o"></i> Level One
-                <span class="pull-right-container">
-                  <i class="fa fa-angle-left pull-right"></i>
-                </span>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="#"><i class="fa fa-files-o"></i> Level Two</a></li>
-                <li class="treeview">
-                  <a href="#"><i class="fa fa-files-o"></i> Level Two
-                    <span class="pull-right-container">
-                      <i class="fa fa-angle-left pull-right"></i>
-                    </span>
-                  </a>
-                  <ul class="treeview-menu">
-                    <li><a href="#"><i class="fa fa-files-o"></i> Level Three</a></li>
-                    <li><a href="#"><i class="fa fa-files-o"></i> Level Three</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li><a href="#"><i class="fa fa-files-o"></i> Level One</a></li>
+            <li><a href="?p=orderhistory"><i class="fa fa-book"></i><span>Order History</span></a></li>
           </ul>
         </li>
-        -->       
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-laptop"></i>
+            <span>Truck</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="?p=truck&pil=list"><i class="fa fa-book"></i><span>List Truck</span></a></li>
+            <li><a href="?p=truck&pil=reg"><i class="fa fa-book"></i><span>Tambah Armada</span></a></li>
+          </ul>
+        </li> 
+      <?php }?>
+      <?php if ($seskdlvl=='customer'){?>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-laptop"></i>
+            <span>Overview</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="?p=order&pil=list"><i class="fa fa-book"></i><span>Order History</span></a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-laptop"></i>
+            <span>Order</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="?p=order&pil=reg"><i class="fa fa-book"></i><span>Create Order</span></a></li>
+          </ul>
+        </li> 
+      <?php }?>
+        <li><a href="logout.php"><i class="fa fa-laptop"></i><span>Logout</span></a></li>
+        
       </ul>
     </section>
     <!-- /.sidebar -->
